7861e938e4bd325fcf0388e6751a71d48e7ff67c
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-04-12 21:33:31 -0300

    Projeto m03 upado

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+//Configurando nossa aplicaçao para trabalhar com json
+app.use(express.json());
+
+app.use(cors());
+
+// Definindo dados para usar na aplicacao
+
+const paletas = [
+    {
+        id: 1,
+        sabor: 'Açai com Leite Condensado',
+        descricao: 'Açai com Leite Condensado',
+        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+        preco: 10.0,
+      },
+      {
+        id: 2,
+        sabor: 'Saco de macho',
+        descricao: 'Haha, eh mole?',
+        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+        preco: 10.0,
+      },
+];
+
+app.get('/', (req,res)=>{
+ res.send("Salve porra");
+});
+
+app.get('/paletas/find-paletas', (req,res)=>{
+res.send(paletas)
+});
+
+app.listen(port, () =>{
+    console.log(`A aplicação está rodando em http://localhost:${port}`);
+});
