4996d519915c01bedbf6494bddb45d3b2a3bbd88
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-04-13 19:59:40 -0300

    Feat route find subject by id index

--- index.js
+++ index.js
@@ -5,35 +5,37 @@ const app = express();
 //Configurando nossa aplicaçao para trabalhar com json
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
 
 // Definindo dados para usar na aplicacao
 
 const paletas = [
-    {
-        id: 1,
-        sabor: 'Açai com Leite Condensado',
-        descricao: 'Açai com Leite Condensado',
-        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-        preco: 10.0,
-      },
-      {
-        id: 2,
-        sabor: 'Saco de macho',
-        descricao: 'Haha, eh mole?',
-        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-        preco: 10.0,
-      },
+  {
+    id: 1,
+    sabor: "Açai com Leite Condensado",
+    descricao: "Açai com Leite Condensado",
+    foto: "https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg",
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: "Saco de macho",
+    descricao: "Haha, eh mole?",
+    foto: "https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg",
+    preco: 10.0,
+  },
 ];
 
-app.get('/', (req,res)=>{
- res.send("Salve porra");
+app.get("/", (req, res) => {
+  res.send("Salve porra");
 });
 
-app.get('/paletas/find-paletas', (req,res)=>{
-res.send(paletas)
+app.get("/paletas/find-paletas/:id", (req, res) => {
+  const idParam = req.params.id;
+  const chosenPaleta = paletas.find((paleta) => paleta.id == idParam);
+  res.send(chosenPaleta);
 });
 
-app.listen(port, () =>{
-    console.log(`A aplicação está rodando em http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`A aplicação está rodando em http://localhost:${port}`);
 });
