7782bdf62b9fdb52130a3161633635ce7aa0be40
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-04-16 13:03:41 -0300

    Feat: added new object to the API. Object description

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const characters = [
     standstatus: "BB",
     habilidades: "Parar o tempo",
     foto: "https://mugenarchive.com/forums/576546d5bf292b9424d94fde84f7b5a3/images/jotaro_47522_thumb.gif",
+    descricao: "Jotaro Kujo, um dos usuários de stand mais fortes dentro do universo de Jojo's Bizarre Adventures. Utilizando do poder de seu Stand Star Platinum, pode parar o tempo com uma duração definida (Sendo a mais longa a duração de 5 segundos). "
   },
   {
     id: 2,
@@ -26,6 +27,7 @@ const characters = [
     standstatus: "CC",
     habilidades: "Parar o tempo (WRYYY)",
     foto: "https://image.myanimelist.net/ui/HlQ6Ym2sGbLFJFCFIz0ba-MuuU8Ua6BvgxzQMx8j6l2CuPleFNp8y-YhmDGoXkzU-uYo0Sc7wv5IKkwcVce7oLUhmUcifn8wfJZlPUwTf_IjffB0JfIPJKNl-IqIUnZWg8WyYxioYgbadySMT3keEXOCVG2ALsTueKfmkx7vh3E",
+    descricao:"Dio Brando, assim como o inimigo que o derrotou, era extremamente poderoso. Tal poder não vinha só de seu Stand, mas também de suas capacidades sobrehumanas que lhes foram presenteadas através da máscara de pedra, que lhe transformou em um vampiro."
   },
 ];
 
